cfbe6a41cf8da8227289780f9ddc6af4c62c2a2b
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-02-27 09:31:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 bool checkPrime(uint64_t value) {
-	// вставьте код функции
 	int d = 1;
 	for (int i = 1; i <= value / 2; i++)
 		if (value % i == 0)
@@ -18,7 +17,6 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-	// вставьте код функции
 	int d, a=0;
 	int mas[100];
 	for (int i = 2; i < 100; i++)
@@ -37,7 +35,6 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-	// вставьте код функции
 	int d;
 	for (int i = value + 1; i < value + 100; i++)
 	{
@@ -51,7 +48,6 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	// вставьте код функции
 	int d, s = 0;
 	for (uint64_t i = 2; i < hbound; i++)
 	{
