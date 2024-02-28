7a699eea1bdbdcaddc43c12e3ffb4bafc92776a3
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-02-28 19:56:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 NNTU-CS
+/ Copyright 2022 NNTU-CS
 #include <iostream>
 #include <cstdint>
 #include "alg.h"
@@ -6,72 +6,61 @@
 using namespace std;
 
 bool checkPrime(uint64_t value) {
-	int d = 1;
-	for (int i = 1; i <= value / 2; i++)
-		if (value % i == 0)
-			d++;
-	if (d > 2)
-		return false;
-	else
-		return true;
+// вставьте код функции
+  int d = 1;
+  for (int i = 1; i <= value / 2; i++)
+    if (value % i == 0)
+      d++;
+  if (d != 2)
+    return false;
+  else
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	int d, a=0;
-	int mas[100];
-	for (int i = 2; i < 100; i++)
-	{
-		d = 1;
-		for (int j = 1; j <= i / 2; j++)
-			if (i % j == 0)
-				d++;
-		if (d == 2)
-		{
-			mas[a] = i;
-			a++;
-		}
-	}
-	return mas[n-1];
+// вставьте код функции
+  int a = 1, value = 1;
+  while (a <= n) {
+    if (checkPrime(value) == true)
+      a++;
+    value++;
+  }
+  return (value-1);
 }
 
 uint64_t nextPrime(uint64_t value) {
-	int d;
-	for (int i = value + 1; i < value + 100; i++)
-	{
-		d = 1;
-		for (int j = 1; j <= i / 2; j++)
-			if (i % j == 0)
-				d++;
-		if (d == 2)
-			return i;
-	}
+// вставьте код функции
+  int j = value;
+  while (true) {
+    j++;
+    if (checkPrime(j) == true)
+      return j;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	int d, s = 0;
-	for (uint64_t i = 2; i < hbound; i++)
-	{
-		d = 1;
-		for (int j = 1; j <= i / 2; j++)
-			if (i % j == 0)
-				d++;
-		if (d == 2)
-			s += i;
-	}
-	return s;
+// вставьте код функции
+  int j = 1, s=0;
+  while (j < hbound) {
+    if (checkPrime(j) == true)
+      s += j;
+    j++;
+  }
+  return s;
 }
 
 int main()
 {
-	uint64_t value;
-	uint64_t n;
-	uint64_t hbound;
-	cin >> value;
-	cout<<checkPrime(value)<<endl;
-	cin >> n;
-	cout << nPrime(n) << endl;
-	cin >> value;
-	cout << nextPrime(value) << endl;
-	cin >> hbound;
-	cout << sumPrime(hbound) << endl;
+  uint64_t value;
+  uint64_t n;
+  uint64_t hbound;
+  cin >> value;
+  cout<<checkPrime(value)<<endl;
+  cin >> n;
+  cout << nPrime(n) << endl;
+  cin >> value;
+  cout << nextPrime(value) << endl;
+  cin >> hbound;
+  cout << sumPrime(hbound) << endl;
+  return 0;
 }
