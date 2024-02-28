ca29b3f1313063fcf8b4c9cbde4bb10d626afacb
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-02-28 14:25:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,7 +11,7 @@ bool checkPrime(uint64_t value) {
   for (int i = 1; i <= value / 2; i++)
     if (value % i == 0)
       d++;
-  if (d > 2)
+  if (d != 2)
     return false;
   else
     return true;
@@ -19,46 +19,32 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
-  int d, a=0;
-  int mas[100];
-  for (int i = 2; i < 100; i++) {
-    d = 1;
-    for (int j = 1; j <= i / 2; j++)
-      if (i % j == 0)
-        d++;
-      if (d == 2) {
-        mas[a] = i;
-        a++;
-    }
+  int a = 1, value = 1;
+  while (a <= n) {
+    if (checkPrime(value) == true)
+      a++;
+    value++;
   }
-  return mas[n-1];
+  return (value-1);
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-  int d;
-  for (int i = value + 1; i < value + 100; i++)
-  {
-    d = 1;
-    for (int j = 1; j <= i / 2; j++)
-      if (i % j == 0)
-        d++;
-    if (d == 2)
-      return i;
+  int j = value;
+  while (true) {
+    j++;
+    if (checkPrime(j) == true)
+      return j;
   }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-  int d, s = 0;
-  for (uint64_t i = 2; i < hbound; i++)
-  {
-    d = 1;
-    for (int j = 1; j <= i / 2; j++)
-      if (i % j == 0)
-        d++;
-    if (d == 2)
-      s += i;
+  int j = 1, s=0;
+  while (j < hbound) {
+    if (checkPrime(j) == true)
+      s += j;
+    j++;
   }
   return s;
 }
@@ -76,4 +62,5 @@ int main()
   cout << nextPrime(value) << endl;
   cin >> hbound;
   cout << sumPrime(hbound) << endl;
+  return 0;
 }
