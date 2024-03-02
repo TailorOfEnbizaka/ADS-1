76db064e45321efa8827fe7fe607ae7bf7bd38d2
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-03-02 13:59:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,14 +3,14 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-  int d = 1;
-  for (int i = 1; i <= value / 2; i++)
-    if (value % i == 0)
-      d++;
-  if (d != 2)
-    return false;
-  else
+  for (uint64_t i = 2; i <= value / 2; i++)
+    if (value % i == 0) {
+      return false;
+    }
+  if (value > 1)
     return true;
+  else
+    return false;
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -36,7 +36,7 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-  int j = 1, s=0;
+  uint64_t j = 1, s = 0;
   while (j < hbound) {
     if (checkPrime(j) == true)
       s += j;
